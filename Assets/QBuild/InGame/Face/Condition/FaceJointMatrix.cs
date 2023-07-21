0dc10851cd2858ca23efc330b6fc7e4b9f87a245
LIces <ice.chiruno9@gmail.com>
2023-07-21 22:28:09 +0900

    ファイル 移動

--- Assets/QBuild/InGame/Face/Condition/FaceJointMatrix.cs
+++ Assets/QBuild/InGame/Face/Condition/FaceJointMatrix.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using SherbetInspector.Core.Attributes;
-using SherbetInspector.Script.Serializable;
+using SherbetInspector.Serializable;
 using UnityEditor;
 using UnityEngine;
 
--- Assets/SherbetInspector/Script/Serializable/SerializableDictionary.cs
+++ Assets/SherbetInspector/Script/Serializable/SerializableDictionary.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Diagnostics;
 using UnityEngine;
 
-namespace SherbetInspector.Script.Serializable
+namespace SherbetInspector.Serializable
 {
     [Serializable]
     public class SerializableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IDictionary
