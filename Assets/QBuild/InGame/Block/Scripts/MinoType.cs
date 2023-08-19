c3821914aef27f4c28157aecbd46123f94e54937
ICE <ice.chiruno9@gmail.com>
2023-08-19 20:41:24 +0900

    名前をMinoGeneratorからMinoTypeに変更

---
+++ Assets/QBuild/InGame/Block/Scripts/MinoType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using QBuild.Const;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace QBuild
+{
+    [CreateAssetMenu(fileName = "MinoType", menuName = EditorConst.ScriptablePrePath + "MinoType",
+        order = 0)]
+    public class MinoType : ScriptableObject
+    {
+        [Serializable]
+        public class PositionToBlockGenerator
+        {
+            [SerializeField] public Vector3Int _pos;
+            [SerializeField] public BlockType _blockType;
+        }
+
+        [SerializeField] private List<PositionToBlockGenerator> _blockTypes;
+
+        public IReadOnlyList<PositionToBlockGenerator> GetBlockTypes()
+        {
+            return _blockTypes;
+        }
+    }
+}
