c662f7259d806ec203673aac86ade4a45923faad
ICE <ice.chiruno9@gmail.com>
2023-08-19 20:37:40 +0900

    Editor拡張 Toolbarのpre Pathを定数化

---
+++ Assets/QBuild/Const/EditorConst.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace QBuild.Const
+{
+    public static class EditorConst
+    {
+        /// <summary>
+        /// QBuild用のScriptableObjectのパスの先頭
+        /// </summary>
+        public const string ScriptablePrePath = "Tools/QBuild/";
+
+        /// <summary>
+        /// QBuild用のScriptableObjectのVariableのパスの先頭
+        /// </summary>
+        public const string VariablePrePath = "Tools/QBuild/Variable/";
+        
+        /// <summary>
+        /// 重要度の低い順に並べるための定数
+        /// </summary>
+        public const int OtherOrder = 11;
+    }
+}
