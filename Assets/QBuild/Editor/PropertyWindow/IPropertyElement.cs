58ae67e9d935ccab4a3efed872fc744be8710ae6
ICE <ice.chiruno9@gmail.com>
2023-08-29 07:20:10 +0900

    ゲームプロパティ用エディタウィンドウの作成

---
+++ Assets/QBuild/Editor/PropertyWindow/IPropertyElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using QBuild.Camera;
+using UnityEditor;
+
+namespace QBuild.PropertyWindow
+{
+    public interface IPropertyElement
+    {
+        public string DisplayName { get; }
+    };
+
+    public class ScriptableTreeElement : IPropertyElement
+    {
+        public Type Type { get; }
+
+        public string DisplayName { get; }
+
+        public ScriptableTreeElement(Type type)
+        {
+            Type = type;
+            DisplayName = type.Name;
+        }
+        
+        public ScriptableTreeElement(Type type, string displayName)
+        {
+            Type = type;
+            DisplayName = displayName;
+        }
+        
+    }
+
+
+    public class GenreTreeElement : IPropertyElement
+    {
+        private string _name;
+        public string DisplayName => _name;
+        
+        public GenreTreeElement(string displayName)
+        {
+            _name = displayName;
+        }
+    }
+}
---
+++ Assets/QBuild/Editor/PropertyWindow/PropertyEditorList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using QBuild.Camera;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace QBuild.PropertyWindow
+{
+    public class PropertyEditorList : VisualElement
+    {
+        public event Action<ScriptableTreeElement> OnSelectItem; 
+        
+        public PropertyEditorList()
+        {
+            var tree = UIToolkitUtility.GetVisualTree("PropertyWindow/PropertyEditorList");
+            var baseElement = tree.Instantiate();
+            Add(baseElement);
+            Initialize();
+        }
+
+        public new class UxmlFactory : UxmlFactory<PropertyEditorList, UxmlTraits>
+        {
+        }
+
+        private void Initialize()
+        {
+            List<TreeViewItemData<IPropertyElement>> rootItems = new();
+            var id = 0;
+
+            var cameraElement =
+                new TreeViewItemData<IPropertyElement>(id++, new ScriptableTreeElement(typeof(CameraScriptableObject)));
+            rootItems.Add(new TreeViewItemData<IPropertyElement>(id++, new GenreTreeElement("Camera"),
+                new List<TreeViewItemData<IPropertyElement>> { cameraElement }));
+
+            var treeView = this.Q<TreeView>();
+
+            treeView.SetRootItems(rootItems);
+
+            treeView.makeItem = () => new Label();
+            treeView.bindItem = (element, index) =>
+                ((Label)element).text = treeView.GetItemDataForIndex<IPropertyElement>(index).DisplayName;
+            treeView.selectionType = SelectionType.Single;
+            treeView.selectionChanged += selections =>
+            {
+                var selected = selections.FirstOrDefault();
+                if (selected == null)
+                {
+                    return;
+                }
+
+                switch (selected)
+                {
+                    case GenreTreeElement genreTreeElement:
+                        Debug.Log("Genre:" + genreTreeElement.DisplayName);
+                        break;
+                    case ScriptableTreeElement scriptableTreeElement:
+                        Debug.Log("Scriptable:" + scriptableTreeElement.DisplayName);
+                        OnSelectItem?.Invoke(scriptableTreeElement);
+                        break;
+                }
+            };
+        }
+    }
+}
