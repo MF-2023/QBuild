7830d04d340aaaf15297b5d3fce95dc59c18a9c6
ICE <ice.chiruno9@gmail.com>
2023-09-05 15:25:36 +0900

    Blockエディタウィンドウクラス

---
+++ Assets/QBuild/Editor/GameDebugWindow/BlockContactView/BlockGraphWindow.cs
@@ -0,0 +1,28 @@
+﻿// @BlockGraphWindow.cs
+// @brief
+// @author ICE
+// @date 2023/09/05
+// 
+// @details
+
+using QBuild.Const;
+using UnityEditor;
+using UnityEngine;
+
+namespace QBuild.GameDebugWindow.BlockContactView
+{
+    public class BlockGraphWindow : EditorWindow
+    {
+        [MenuItem(EditorConst.WindowPrePath + "BlockGraphWindow")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<BlockGraphWindow>();
+            window.titleContent = new GUIContent("BlockGraphWindow");
+            window.Show();
+        }
+
+        private void CreateGUI()
+        {
+        }
+    }
+}
