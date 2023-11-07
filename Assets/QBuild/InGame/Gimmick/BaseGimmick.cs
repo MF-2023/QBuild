a746cea51c36c5d1b2cf1216473b6d6267a4310d
ICE <ice.chiruno9@gmail.com>
2023-11-07 15:16:37 +0900

    基底クラスの実装

---
+++ Assets/QBuild/InGame/Gimmick/BaseGimmick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace QBuild.Gimmick
+{
+    public abstract class BaseGimmick : MonoBehaviour
+    {
+        public abstract void Active();
+        public abstract void Disable();
+    }
+}
