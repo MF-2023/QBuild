c2aa831faa1b68474a3dc49780c33a866cd71c8a
Pureria <117608289+Pureria@users.noreply.github.com>
2023-11-07 17:36:48 +0900

    トランポリン作成

---
+++ Assets/QBuild/InGame/Gimmick/GimmickTrampoline.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace QBuild.Gimmick
+{
+    public class GimmickTrampoline : BaseGimmick
+    {
+        [SerializeField,Tooltip("目標ポイント")]                                  private Transform _targetPoint;
+        [SerializeField,Tooltip("中央ポイント（nullの場合自動で計算される）")]    private Transform _centerPoint;
+        [SerializeField,Tooltip("中央ポイントが設定しない場合の中央の高さ")]      private float     _jumpHeight = 0;
+        [SerializeField,Tooltip("目標ポイントに到着するまでの時間")]              private float     _time;
+
+        public override void Active()
+        {
+            //プレイヤーを何かしらの方法で取得する
+            Transform HogePlayer = null;
+
+            //TODO::西田::プレイヤーのSimulationを切る
+            Vector3 startPosition = HogePlayer.position;
+            Vector3 endPosition = _targetPoint.position;
+            Vector3 centerPosition;
+            if (_centerPoint != null) centerPosition = _centerPoint.position;
+            else centerPosition = GetCenterPoint(startPosition, endPosition, _jumpHeight);
+
+
+        }
+
+        public override void Disable()
+        {
+        }
+
+        private Vector3 GetCenterPoint(Vector3 startPoint,Vector3 endPoint,float height)
+        {
+            Vector3 ret = Vector3.zero;
+            ret = (endPoint + startPoint) * 0.5f;
+            ret.y += height;
+            return ret;
+        }
+    }
+}
