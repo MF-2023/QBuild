b80fbf0aefba13cd7184bc097573f8b26ab5f3cf
Pureria <117608289+Pureria@users.noreply.github.com>
2023-12-01 17:00:07 +0900

    タイトルシーンの作成

---
+++ Assets/QBuild/InGame/GameCycle/Script/Title/TitleAction.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace QBuild.GameCycle.Title
+{
+    public class TitleAction : MonoBehaviour
+    {
+        [SerializeField] private GameObject _pressPushButtonText = null;
+        [SerializeField] private GameObject _buttons = null;
+        [SerializeField] private Popup _optionPopup = null;
+        
+        private void Start()
+        {
+            ShowPressPushButtonText();
+            _optionPopup.gameObject.SetActive(false);
+        }
+        
+        public void ShowButtons()
+        {
+            _pressPushButtonText.SetActive(false);
+            _buttons.SetActive(true);
+        }
+        
+        public void ShowPressPushButtonText()
+        {
+            _pressPushButtonText.SetActive(true);
+            _buttons.SetActive(false);
+        }
+        
+        public void ShowOptionPopup()
+        {
+            _optionPopup.gameObject.SetActive(true);
+            _optionPopup.ShowPopup();
+        }
+
+        public void HideOptionPopup()
+        {
+            _optionPopup.HidePopup(() => _optionPopup.gameObject.SetActive(false));
+        }
+    }
+}
