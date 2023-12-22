4282aa8a7d4088d8d160b1273d5caa0129523fdf
ICE <ice.chiruno9@gmail.com>
2023-12-22 14:00:59 +0900

    スクロールメニュー

---
+++ Assets/QBuild/UI/ScrollView/ExtensionScrollView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace QBuild.UI.ScrollView
+{
+    public class ExtensionScrollView : UnityEngine.UIElements.ScrollView
+    {
+        public new class UxmlFactory : UxmlFactory<ExtensionScrollView, UxmlTraits>
+        {
+        }
+    }
+}
