1de3ef19a1f395c43d77b1e3149a1c3fb653545e
ICE <ice.chiruno9@gmail.com>
2024-01-10 08:50:38 +0900

    プレイヤーノックバック調整

---
+++ Assets/QBuild/InGame/Gimmick/FireBar/Scripts/GimmickSpin.cs
@@ -0,0 +1,94 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace QBuild.Gimmick
+{
+    public class GimmickSpin : MonoBehaviour
+    {
+        [SerializeField] private float _spinSpeed;
+        [SerializeField] private bool _isOn;
+
+        private bool _isMoving;
+
+        //回転方向
+        private enum SpinDirection
+        {
+            Left,
+            Right
+        }
+
+        [SerializeField] private SpinDirection _spinDirection;
+
+        public void Active()
+        {
+            _isOn = true;
+            OnActiveChanged();
+        }
+
+        public void Disable()
+        {
+            _isOn = false;
+            OnActiveChanged();
+        }
+
+        private void SpinStart()
+        {
+            if(_isMoving) return;
+            SpinAsync().Forget();
+        }
+
+        private async UniTask SpinAsync()
+        {
+            _isMoving = true;
+            
+            while (_isMoving || _isOn)
+            {
+                var lastAnglesY = transform.localRotation.eulerAngles.y;
+                switch (_spinDirection)
+                {
+                    case SpinDirection.Left:
+                        transform.Rotate(0, _spinSpeed * Time.deltaTime, 0);
+                        break;
+                    case SpinDirection.Right:
+                        transform.Rotate(0, -_spinSpeed * Time.deltaTime, 0);
+                        break;
+                }
+                var currentAnglesY = transform.localRotation.eulerAngles.y;
+                if (!_isOn)
+                {
+                    //90度毎に回転を止める
+                    if (Mathf.FloorToInt(lastAnglesY / 90) != Mathf.FloorToInt(currentAnglesY / 90))
+                    {
+                        _isMoving = false;
+                    }
+                }
+                
+                await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
+            }
+        }
+
+        private void Update()
+        {
+        }
+
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorApplication.isPlaying)
+            {
+                OnActiveChanged();
+            }
+#endif
+        }
+
+        private void OnActiveChanged()
+        {
+            if (_isOn)
+            {
+                if (!_isMoving) SpinStart();
+            }
+        }
+    }
+}
---
+++ Assets/QBuild/InGame/Player/_Script/PlayerDamage.cs
@@ -0,0 +1,44 @@
+﻿using Cysharp.Threading.Tasks;
+using QBuild.Player.Controller;
+using QBuild.Player.Core;
+using UnityEngine;
+
+namespace QBuild.Player
+{
+    public class PlayerDamage : MonoBehaviour,IDamageable
+    {
+        private PlayerController _playerController;
+        private Movement _movement;
+        
+        //無敵
+        [SerializeField] private bool _invincible = false;
+        [SerializeField] private float _invincibleTime = 0.5f;
+        private float _invincibleTimer = 0f;
+        private void Start()
+        {
+            if (!TryGetComponent<PlayerController>(out _playerController))
+                UnityEngine.Debug.LogError($"{transform.name}にPlayerControllerが存在しません");
+            _playerController.Core.GetCoreComponent<Movement>(ref _movement);
+        }
+        
+        public void Knockback(Vector3 direction, float power)
+        {
+            if(_invincible) return;
+            
+            _movement.AddForce(direction * power, ForceMode.VelocityChange);
+            _invincible = true;
+            _invincibleTimer = 0f;
+            UniTask.Create(InvincibleTimer);
+        }
+        
+        private async UniTask InvincibleTimer()
+        {
+            while (_invincibleTimer < _invincibleTime)
+            {
+                _invincibleTimer += Time.deltaTime;
+                await UniTask.Yield();
+            }
+            _invincible = false;
+        }
+    }
+}
