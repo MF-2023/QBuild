bbb7279d499206f590deeee52d8817c2f7629a40
ICE <ice.chiruno9@gmail.com>
2024-01-11 07:51:46 +0900

    クラス定義

---
+++ Assets/QBuild/InGame/Result/Scripts/ResultEffect.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace QBuild.Result
+{
+    public class ResultEffect : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/QBuild/InGame/Result/Scripts/ResultPopup.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace QBuild.Result
+{
+    public class ResultPopup : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        private void Awake()
+        {
+            if (_animator == null)
+            {
+                Debug.LogError("Animatorが設定されていません", this);
+            }
+        }
+
+        public void Show()
+        {
+        }
+
+        public void Hide()
+        {
+            
+        }
+    }
+}
