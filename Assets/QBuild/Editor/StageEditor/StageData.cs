6e179efbf6898988ac106935a484e36534441e65
TakemuraKimihiro(Laptop) <112789077+Nekunegi@users.noreply.github.com>
2024-01-12 15:03:42 +0900

    ステージ情報のロード、セーブのバグ修正

--- Assets/QBuild/Editor/StageEditor/StageData.cs
+++ Assets/QBuild/Editor/StageEditor/StageData.cs
@@ -6,20 +6,36 @@ using UnityEngine.Serialization;
 
 namespace QBuild.StageEditor
 {
+    /// <summary>
+    /// ステージに関するデータ
+    /// </summary>
     [CreateAssetMenu(menuName = "Tools/QBuild/StageEditor/StageData")]
     public class StageData : ScriptableObject
     {
         public GameObject _stage;
-        public AssetReferenceGameObject GetReferenceStagePrefab()
-        {
-            return null;
-        }
-        public string _fileName;
-        public string _stageName;
-        public int _stageDifficult;
-        public int _crystalCount;
-        public Texture2D _stageImage;
-        public Vector3Int _stageArea;
-    }
+        [SerializeField] private bool _isExistWarningItem;
+        public bool IsExistWarningItem() => _isExistWarningItem;
 
+        [SerializeField] private AssetReferenceGameObject _stagePrefab;
+        public AssetReferenceGameObject GetStagePrefab() => _stagePrefab;
+
+        [SerializeField] private string _fileName;
+        public string GetFileName() => _fileName;
+
+
+        [SerializeField] private string _stageName;
+        public string GetStageName() => _stageName;
+
+        [SerializeField] private int _stageDifficult;
+        public int GetStageDifficult() => _stageDifficult;
+
+        [SerializeField] private int _crystalCount;
+        public int GetCrystalCount() => _crystalCount;
+
+        [SerializeField] private Texture2D _stageImage;
+        public Texture2D GetStageImage() => _stageImage;
+
+        [SerializeField] private Vector3Int _stageArea;
+        public Vector3Int GetStageArea() => _stageArea;
+    }
 }
