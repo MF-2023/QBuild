ec90e9218cbd76eec9c110f78c565244fb66c53c
TakemuraKimihiro(Laptop) <112789077+Nekunegi@users.noreply.github.com>
2024-01-12 16:31:59 +0900

    ステージエディタのスナップ機能が別シーンでも正しく機能する

--- Assets/QBuild/Editor/StageEditor/ObjectSnapper.cs
+++ Assets/QBuild/Editor/StageEditor/ObjectSnapper.cs
@@ -8,7 +8,9 @@ namespace QBuild.StageEditor
     {
         private const float SnapDistance = 1.0f;
         public static Vector3Int stageArea;
-        public static bool isEnable;
+        public static bool isEnable = true;
+        public static bool isEnableArea = true;
+        public static bool isBlockOnly = true;
 
         static ObjectSnapper()
         {
@@ -17,34 +19,43 @@ namespace QBuild.StageEditor
 
         private static void OnSceneGUI(SceneView sceneView)
         {
-            if (!isEnable)
-                return;
-
-
             var selectedGameObject = Selection.transforms;
 
             foreach (var transform in selectedGameObject)
             {
-                if (transform.gameObject.layer == LayerMask.NameToLayer("Block"))
+                if (isBlockOnly)
                 {
-                    SnapToGrid(transform);
+                    if (transform.gameObject.layer != LayerMask.NameToLayer("Block"))
+                    {
+                        continue;
+                    }
                 }
+
+                SnapToGrid(transform);
             }
         }
 
         public static void SnapToGrid(Transform transform)
         {
             var pos = transform.position;
-            var snapPos = new Vector3(
-                Mathf.Round(pos.x / SnapDistance) * SnapDistance,
-                Mathf.Round(pos.y / SnapDistance) * SnapDistance,
-                Mathf.Round(pos.z / SnapDistance) * SnapDistance
-            );
+            
+            var snapPos = pos;
 
-            //snapPos‚ðstageArea‚Ì”ÍˆÍ“à‚ÉŽû‚ß‚é
-            snapPos.x = Mathf.Clamp(snapPos.x, -stageArea.x / 2.0f, stageArea.x / 2.0f);
-            snapPos.y = Mathf.Clamp(snapPos.y, 0, stageArea.y);
-            snapPos.z = Mathf.Clamp(snapPos.z, -stageArea.z / 2.0f, stageArea.z / 2.0f);
+            if (isEnable)
+            {
+                snapPos = new Vector3(
+                    Mathf.Round(pos.x / SnapDistance) * SnapDistance,
+                    Mathf.Round(pos.y / SnapDistance) * SnapDistance,
+                    Mathf.Round(pos.z / SnapDistance) * SnapDistance
+                );
+            }
+
+            if (isEnableArea)
+            {
+                snapPos.x = Mathf.Clamp(snapPos.x, -stageArea.x / 2.0f, stageArea.x / 2.0f);
+                snapPos.y = Mathf.Clamp(snapPos.y, 0, stageArea.y);
+                snapPos.z = Mathf.Clamp(snapPos.z, -stageArea.z / 2.0f, stageArea.z / 2.0f);
+            }
 
             transform.position = snapPos;
         }
