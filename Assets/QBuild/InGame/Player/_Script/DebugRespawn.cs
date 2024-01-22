cb1e2e95f831de27dc7007dcda576052b6226be6
ICE <ice.chiruno9@gmail.com>
2024-01-22 22:41:27 +0900

    モデルのダウンロード

--- Assets/QBuild/InGame/Player/_Script/DebugRespawn.cs
+++ Assets/QBuild/InGame/Player/_Script/DebugRespawn.cs
@@ -1,9 +1,17 @@
+using System;
 using UnityEngine;
 
 namespace QBuild.Player.Debug
 {
     public class DebugRespawn : MonoBehaviour
     {
+        [SerializeField] private Vector3 _position;
+        
+        private void Awake()
+        {
+            _position = transform.position;
+        }
+
         private void Update()
         {
             if (transform.position.y < -5.0f)
