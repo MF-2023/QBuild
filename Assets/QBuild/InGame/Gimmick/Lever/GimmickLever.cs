c0d2c286359ddf1911e0bbd7ca641e15478a9e1d
Pureria <117608289+Pureria@users.noreply.github.com>
2024-02-08 06:28:18 +0900

    レバーの音が一回しかならないように変更

--- Assets/QBuild/InGame/Gimmick/Lever/GimmickLever.cs
+++ Assets/QBuild/InGame/Gimmick/Lever/GimmickLever.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace QBuild.Gimmick
@@ -9,11 +10,24 @@ namespace QBuild.Gimmick
         [SerializeField,Tooltip("レバーがアクティブになったときのイベント")]   private UnityEvent _ActiveLever;
         [SerializeField] private SimpleAnimator _animation;
         [SerializeField] private AudioSource _audioSource;
+
+        private bool _isActive;
+
+        private void Awake()
+        {
+            _isActive = false;
+        }
+
         public override void Active()
         {
             _animation.graph.Play();
-            _audioSource.Play();
             _ActiveLever?.Invoke();
+
+            if (!_isActive)
+            {
+                _isActive = true;
+                _audioSource.Play();
+            }
         }
 
         public override void Disable()
